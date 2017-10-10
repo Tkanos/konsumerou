1529fa39e251428a5ebdcc7e4abad41ea9cb3657
Felipe Dutra Tine e Silva <Tkanos@users.noreply.github.com>
2017-10-10 17:08:46 +0200

    Update listener.go

--- listener.go
+++ listener.go
@@ -3,46 +3,93 @@ package konsumerou
 import (
 	"context"
 	"errors"
-	"testing"
+	"fmt"
+	"os"
+	"strings"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/Shopify/sarama"
+	"github.com/bsm/sarama-cluster"
 )
 
-func Test_NewServiceTracing_Should_Create_New_Service_Instance(t *testing.T) {
-	//Arrange
-	fake := new(mockedService)
+//Handler that handler kafka messages received
+type Handler func(ctx context.Context, msg *sarama.ConsumerMessage) error
 
-	//Act
-	tracer := NewServiceTracing(fake)
-
-	//Assert
-	assert.NotNil(t, tracer)
+// listener object represents kafka customer
+type listener struct {
+	consumer *cluster.Consumer
 }
 
-func Test_Tracing_Track_Should_Go_Throught_The_Method(t *testing.T) {
-	//Arrange
-	fake := new(mockedService)
-	model := MyServiceMessage{MyServiceID: id}
-	fake.On("Track", context.Background(), model).Return(nil)
-	tracer := NewServiceTracing(fake)
-
-	//Act
-	err := tracer.Track(context.Background(), model)
-
-	//Assert
-	assert.NoError(t, err)
+// Listener ...
+type Listener interface {
+	Subscribe(handler Handler, exit chan bool) error
+	Close()
 }
 
-func Test_Tracing_Track_With_Error_Should_Go_Throught_The_Method(t *testing.T) {
-	//Arrange
-	fake := new(mockedService)
-	model := MyServiceMessage{MyServiceID: id}
-	fake.On("Track", context.Background(), model).Return(errors.New("test"))
-	tracer := NewServiceTracing(fake)
+// NewListener ...
+func NewListener(brokers []string, groupID string, topicList string, offset int64) (Listener, error) {
+	if brokers == nil || len(brokers) == 0 {
+		return nil, errors.New("cannot create new listener, brockers cannot be empty")
+	}
+	if groupID == "" {
+		return nil, errors.New("cannot create new listener, groupID cannot be empty")
+	}
+	if topicList == "" {
+		return nil, errors.New("cannot create new listener, topicList cannot be empty")
+	}
 
-	//Act
-	err := tracer.Track(context.Background(), model)
+	// Init config
+	config := cluster.NewConfig()
 
-	//Assert
-	assert.Error(t, err)
+	//config.Logger = logger //verbose mode
+	config.Consumer.Return.Errors = true
+	config.Group.Return.Notifications = true
+	config.Consumer.Offsets.Initial = offset
+
+	// Init consumer, consume errors & messages
+	consumer, err := cluster.NewConsumer(brokers, groupID, strings.Split(topicList, ","), config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &listener{
+		consumer: consumer,
+	}, nil
+}
+
+func (l *listener) Subscribe(handler Handler, exit chan bool) error {
+	if l.consumer == nil {
+		return errors.New("cannot subscribe. Customer is nil")
+	}
+
+	go func() {
+		// Consume all channels, wait for signal to exit
+		for {
+			select {
+			case msg, more := <-l.consumer.Messages():
+				if more {
+					if handler(context.Background(), msg) == nil {
+						l.consumer.MarkOffset(msg, "")
+					}
+				}
+			case ntf, more := <-l.consumer.Notifications():
+				if more {
+					fmt.Fprintf(os.Stdout, "Rebalanced: %+v\n", ntf)
+				}
+			case err, more := <-l.consumer.Errors():
+				if more {
+					fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+				}
+			case <-exit:
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (l *listener) Close() {
+	if l.consumer != nil {
+		l.consumer.Close()
+	}
 }
