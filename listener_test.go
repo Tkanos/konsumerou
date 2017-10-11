5764ac8b39e474572bab40ca453d6782561f7b28
Felipe Dutra Tine e Silva <tkano@gmail.com>
2017-10-11 10:17:56 +0200

    fix unit test

--- listener_test.go
+++ listener_test.go
@@ -7,19 +7,19 @@ import (
 )
 
 func Test_NewListener_EmptyBrocker(t *testing.T) {
-	listener, err := NewListener([]string{}, "my-topic", "group-id", 0)
+	listener, err := NewListener([]string{}, "my-topic", "group-id", 0, nil)
 	assert.Error(t, err)
 	assert.Nil(t, listener)
 }
 
 func Test_NewListener_EmptyGroup(t *testing.T) {
-	listener, err := NewListener([]string{"brocker1", "brocker2"}, "", "my-topic", 0)
+	listener, err := NewListener([]string{"brocker1", "brocker2"}, "", "my-topic", 0, nil)
 	assert.Error(t, err)
 	assert.Nil(t, listener)
 }
 
 func Test_NewListener_EmptyTopicList(t *testing.T) {
-	listener, err := NewListener([]string{"brocker1", "brocker2"}, "group-id", "", 0)
+	listener, err := NewListener([]string{"brocker1", "brocker2"}, "group-id", "", 0, nil)
 	assert.Error(t, err)
 	assert.Nil(t, listener)
 }
