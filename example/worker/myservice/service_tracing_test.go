ad93e3cf8baf419744a993716946bb06f92a6688
Felipe Dutra Tine e Silva <Tkanos@users.noreply.github.com>
2017-12-06 10:11:46 +0100

    fix opentracing tests

--- example/worker/myservice/service_tracing_test.go
+++ example/worker/myservice/service_tracing_test.go
@@ -18,29 +18,29 @@ func Test_NewServiceTracing_Should_Create_New_Service_Instance(t *testing.T) {
 }
 
 func Test_Tracing_ProcessMessage_Should_Go_Throught_The_Method(t *testing.T) {
-	/*	//Arrange
-		fake := new(mockedService)
-		model := MyServiceMessageProcessor{MyServiceID: "123"}
-		fake.On("ProcessMessage", context.Background(), &model).Return(nil)
-		tracer := NewServiceTracing(fake)
+	//Arrange
+	fake := new(mockedService)
+	model := MyServiceMessageProcessor{MyServiceID: "123"}
+	fake.On("ProcessMessage", mock.Anything, &model).Return(nil)
+	tracer := NewServiceTracing(fake)
 
-		//Act
-		err := tracer.ProcessMessage(context.Background(), &model)
+	//Act
+	err := tracer.ProcessMessage(context.Background(), &model)
 
-		//Assert
-		assert.NoError(t, err) */
+	//Assert
+	assert.NoError(t, err) 
 }
 
 func Test_Tracing_ProcessMessage_With_Error_Should_Go_Throught_The_Method(t *testing.T) {
-	/*	//Arrange
-		fake := new(mockedService)
-		model := MyServiceMessageProcessor{MyServiceID: "123"}
-		fake.On("ProcessMessage", context.Background(), &model).Return(errors.New("test"))
-		tracer := NewServiceTracing(fake)
+	//Arrange
+	fake := new(mockedService)
+	model := MyServiceMessageProcessor{MyServiceID: "123"}
+	fake.On("ProcessMessage", mock.Anything, &model).Return(errors.New("test"))
+	tracer := NewServiceTracing(fake)
 
-		//Act
-		err := tracer.ProcessMessage(context.Background(), &model)
+	//Act
+	err := tracer.ProcessMessage(context.Background(), &model)
 
-		//Assert
-		assert.Error(t, err) */
+	//Assert
+	assert.Error(t, err) 
 }
